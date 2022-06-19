f0c70c0400095fbc60efe98040a60cb3818b1be0
lazywon <jinjooisme@gmail.com>
2022-06-19 12:22:59 +0900

    Search Header 생성

---
+++ src/components/videoDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoDetail = () => {
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    );
+};
+
+export default VideoDetail;
