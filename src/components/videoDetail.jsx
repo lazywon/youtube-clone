e1814d334c222401e12225da3d3b562b1fcf0003
lazywon <jinjooisme@gmail.com>
2022-06-19 18:04:46 +0900

    특정 비디오 클릭시 비디오 출력되도록 변경

--- src/components/videoDetail.jsx
+++ src/components/videoDetail.jsx
@@ -4,19 +4,21 @@ const VideoDetail = ({videoDetail}) => {
     const srcUrl = "https://www.youtube.com/embed/" + videoDetail.id;
     return (
         <div className="video-detail">
-            <iframe 
-                id={videoDetail.id} 
-                type="text/html"
-                width="720" 
-                height="405"
-                src={srcUrl}
-                frameBorder="0" 
-                allowFullScreen
-            />
-            <h2>{videoDetail.snippet.title}</h2>
+            {videoDetail.id && 
+                <iframe 
+                    id={videoDetail.id} 
+                    type="text/html"
+                    width="720" 
+                    height="405"
+                    src={srcUrl}
+                    frameBorder="0" 
+                    allowFullScreen
+                />
+            }
+            <h2>{videoDetail.snippet && videoDetail.snippet.title}</h2>
             <span>
-                <p>{videoDetail.snippet.publishedAt}</p>
-                <p>{videoDetail.snippet.description}</p>
+                <p>{videoDetail.snippet && videoDetail.snippet.publishedAt}</p>
+                <p>{videoDetail.snippet && videoDetail.snippet.description}</p>
             </span>
         </div>
     );
