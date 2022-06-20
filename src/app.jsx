669f8fdfc470e88bb6fb96aa3f248703aa6492c8
lazywon <jinjooisme@gmail.com>
2022-06-20 22:00:36 +0900

    feat: [리팩토링] 검색 기능 리팩토링 및 api key 숨기기

--- src/app.jsx
+++ src/app.jsx
@@ -4,36 +4,16 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-const App = () => {
+const App = ({ youtube }) => {
     const [videos, setVideos] = useState([]);
     const search = query => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-        
-        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBOb5mU4mqtqIFS7S0ybtLk5UhYSoxSyP4`, requestOptions)
-        .then(response => response.json())
-        .then(result => 
-            result.items.map(item => ({ ...item, id: item.id.videoId }))
-        )
-        .then(items => setVideos(items))
-        .catch(error => console.log('error', error));
+        youtube.search(query)
+        .then(videos => setVideos(videos));
     };
 
     useEffect(() => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-        
-        fetch(
-            "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBOb5mU4mqtqIFS7S0ybtLk5UhYSoxSyP4", 
-            requestOptions
-        )
-        .then(response => response.json())
-        .then(result => setVideos(result.items))
-        .catch(error => console.log('error', error));
+        youtube.mostPopular()
+        .then(videos => setVideos(videos));
     }, []);
 
     return (
