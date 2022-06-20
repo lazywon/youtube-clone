4742fb3c84d3be0a14eaaad821db1d63482bc6fa
lazywon <jinjooisme@gmail.com>
2022-06-20 20:59:06 +0900

    feat: [리팩토링] video item 생성

--- src/app.jsx
+++ src/app.jsx
@@ -1,61 +1,31 @@
 
 import React, { useCallback, useState, useEffect } from 'react';
-import axios from 'axios';
 import './app.css';
-import SearchHeader from './components/searchHeader';
-import VideoList from './components/videoList';
-import VideoDetail from './components/videoDetail';
+import VideoList from './components/video_list/video_list';
 
 const App = () => {
-    const [videoList, setVideoList] = useState([]);
-    const [videoDetail, setVideoDetail] = useState({});
+    const [videos, setVideos] = useState([]);
 
     useEffect(() => {
-        async function showPoupularList() {
-            try {
-                const response = await axios.get(
-                    'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=AIzaSyBOb5mU4mqtqIFS7S0ybtLk5UhYSoxSyP4'
-                );
-                const popularVideoList = response.data.items;
-                // setVideoList(videoList => [...videoList, popularVideoList]);
-                setVideoList(popularVideoList);
-            } catch (e) {
-                console.log(e);
-            }
-        }
-        showPoupularList();
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        
+        fetch(
+            "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBOb5mU4mqtqIFS7S0ybtLk5UhYSoxSyP4", 
+            requestOptions
+        )
+        .then(response => response.json())
+        .then(result => setVideos(result.items))
+        .catch(error => console.log('error', error));
     }, []);
-
-    const searchVideoList = useCallback(async keyword => {
-        try {
-            const baseUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=AIzaSyBOb5mU4mqtqIFS7S0ybtLk5UhYSoxSyP4'
-            const searchApiUrl = baseUrl + '&q=' + keyword;
-            const response = await axios.get(
-                searchApiUrl
-            );
-            const searchVideoList = response.data.items;
-            setVideoList(searchVideoList);
-        } catch (e) {
-            console.log(e);
-        }
-    }, []);
-
-    const handleVideoDetail = useCallback((video) => {
-        setVideoDetail(video);
-    }, []) ;
 
     return (
         <>
-            <SearchHeader
-                onSearch={searchVideoList} 
-            />
-            <VideoDetail videoDetail={videoDetail}/>
-            <VideoList 
-                videoList={videoList}
-                onClick={handleVideoDetail}
-            />
+            <VideoList videos={videos} />
         </>
-    );
+    )
 };
 
 export default App;
