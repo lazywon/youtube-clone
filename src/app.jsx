6bae8fd1fb50d548a6aa8b9ac8554739428092f4
lazywon <jinjooisme@gmail.com>
2022-06-24 21:38:59 +0900

    feat : 성능 최적화

--- src/app.jsx
+++ src/app.jsx
@@ -1,56 +1,56 @@
-
 import React, { useState, useEffect } from 'react';
+import { useCallback } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
-const App = ({ youtube }) => {
-    const [videos, setVideos] = useState([]);
-    const [selectedVideo, setSelectedVideo] = useState(null);
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-    const selectVideo = video => {
-        setSelectedVideo(video);
-    };
+  const selectVideo = useCallback((video) => {
+    setSelectedVideo(video);
+  }, []);
 
-    const search = query => {
-        setSelectedVideo(null);
-        // loading spinner
-        youtube
-        .search(query)
-        .then(videos => {
-            setVideos(videos);
-            setSelectedVideo(null);
+  const search = useCallback(
+    (query) => {
+      setSelectedVideo(null);
+      // loading spinner
+      youtube
+        .search(query) //
+        .then((videos) => {
+          setVideos(videos);
+          setSelectedVideo(null);
         })
-        .catch(error => alert(error));
-    };
+        .catch((error) => alert(error));
+    },
+    [youtube],
+  );
 
-    useEffect(() => {
-        youtube
-        .mostPopular()
-        .then(videos => setVideos(videos));
-    }, []);
+  useEffect(() => {
+    youtube.mostPopular().then((videos) => setVideos(videos));
+  }, [youtube]);
 
-    return (
-        <div className={styles.app}>
-            <SearchHeader onSearch={search} />
-            <section className={styles.content}>
-                {selectedVideo && (
-                    <div className={styles.detail}>
-                        <VideoDetail video={selectedVideo}/>
-                    </div>
-                )}
-                <div className={styles.list}>
-                    <VideoList 
-                        videos={videos} 
-                        onVideoClick={selectVideo} 
-                        display={selectedVideo ? 'list' : 'grid'}
-                    />
-                </div>
-            </section>
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
         </div>
-    );
+      </section>
+    </div>
+  );
 }
 
 export default App;
-
